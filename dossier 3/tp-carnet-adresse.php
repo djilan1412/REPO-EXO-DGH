6808ff0c77beed2aa50a09f055515b0a6f2dd219
djilan ghoorun <djilan.mrtus@gmail.com>
2025-11-18 11:33:13 +0100

    tp carnet adresse add

---
+++ dossier 3/tp-carnet-adresse.php
@@ -0,0 +1,17 @@
+<?php 
+$contacts = ["Alice Dupont", "John Doe", "Jean Martin", "Marie Curie"];
+ 
+$existants = file("contact.txt", FILE_IGNORE_NEW_LINES);
+ 
+foreach ($contacts as $c) {
+    if (!in_array($c, $existants)) {
+        $existants[] = $c;
+    }
+}
+ 
+file_put_contents("contact.txt", implode("<br>", $existants));
+ 
+echo " Nouveau contact ajouté. <br>";
+$contacts_fini = file_get_contents("contact.txt");
+echo "<br> Contenu fichier contact.txt :<br>" . $contacts_fini;
+?>
